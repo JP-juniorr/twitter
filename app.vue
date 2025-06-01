e51b30b190291a6163e0919ffd9abcb8745d30bf
JP-juniorr <jp.noname@icloud.com>
2025-06-01 15:08:23 +0200

    First Commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-red-500">Twitter Clone</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/css/variables.scss";
+h1 {
+  color: $dim_100; // Using the color variable
+  font-size: 2rem;
+}
+</style>
