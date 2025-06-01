4250a5d5bd53a1986b62c722c0a401b431710bdd
JP-juniorr <jp.noname@icloud.com>
2025-06-01 17:27:16 +0200

    update the navbar

--- app.vue
+++ app.vue
@@ -1,13 +1,42 @@
+<script setup>
+import Left from "./components/sidebar/Left.vue";
+import { ref, watch } from "vue";
+
+const darkMode = ref(true);
+</script>
+
 <template>
-  <div>
-    <h1 class="text-red-500">Twitter Clone</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left Sidebar -->
+          <div
+            class="hidden border-2 border md:block xs-col-span-1 xl:col-span-2"
+          >
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Middle Content -->
+          <div class="bg-blue-500 col-span-2">
+            <h1>middle</h1>
+          </div>
+          <!-- Right Sidebar -->
+          <div class="bg-gray-500 col-span-2">
+            <h1>right</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-@import "@/assets/css/variables.scss";
-h1 {
-  color: $dim_100; // Using the color variable
-  font-size: 2rem;
+@use "@/assets/styles/variables.scss" as vars;
+.dark {
+  color: vars.$dim_100;
 }
 </style>
---
+++ components/sidebar/Left.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="w-8 h-8">
+    <LogoTwitter />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@/assets/styles/variables.scss" as vars;
+</style>
