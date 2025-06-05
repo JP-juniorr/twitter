1312ed79bbaf91d1701effb272d232f8f6a5ea5b
JP-juniorr <jp.noname@icloud.com>
2025-06-05 08:59:00 +0200

    added premium to the right side

--- components/bars/Right.vue
+++ components/bars/Right.vue
@@ -1,39 +1,33 @@
 <script setup>
 import Button from "../reusable/button.vue";
 import search from "../reusable/search.vue";
+import card from "../reusable/card.vue";
 </script>
 
 <template>
   <div class="main">
-    <div>
-      <search></search>
-
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
-        explicabo, enim perferendis inventore recusandae quas error optio earum?
-        Necessitatibus, quae? Eius quae recusandae, earum exercitationem aliquam
-        mollitia placeat. Unde porro libero, repudiandae ut est modi voluptas id
-        quos hic laboriosam ipsam blanditiis vero doloribus, ratione, dicta
-        laudantium esse molestiae animi? Laborum itaque nostrum iste. Minima
-        sunt maiores ad iste? Repellat, at amet eum dolorum natus ea perferendis
-        sed expedita delectus dolores laboriosam sequi nihil repudiandae
-        asperiores voluptas rerum. Eligendi distinctio neque nisi magnam, et
-        maxime ab fugit fuga molestiae necessitatibus quod iste eum expedita.
-        Unde asperiores cupiditate temporibus quas expedita?
-      </p>
-    </div>
+    <search></search>
+    <card class="card">
+      <template #title> Subscribe to Premium </template>
+      <template #content>
+        Subscribe to unlock new features and if eligible, receive a share of
+        revenue.
+      </template>
+      <template #footer>
+        <Button>Subscribe</Button>
+      </template>
+    </card>
   </div>
 </template>
 
 <style scoped lang="scss">
 @use "@/assets/styles/variables.scss" as vars;
 .main {
-  padding: 20px 0px;
-  display: flex;
-  flex-direction: row;
-  width: 100%;
+  display: block;
   padding: 2rem;
-  font-size: 1rem;
+}
+.card {
+  margin: 15px 0px;
 }
 p {
   font-size: 1rem;
---
+++ components/reusable/card.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Button from "./button.vue";
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-title">
+      <slot name="title">Default Title</slot>
+    </div>
+    <div class="card-content">
+      <slot name="content">Default Content</slot>
+    </div>
+    <div class="card-footer">
+      <slot name="footer">
+        <Button class="button-card">Default Button</Button>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 16px;
+  background-color: #fff;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
+}
+
+.card-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 8px;
+  color: #333;
+}
+
+.card-content {
+  font-size: 1rem;
+  margin-bottom: 16px;
+  color: #666;
+}
+
+.card-footer {
+  text-align: left;
+  width: 40%;
+}
+
+.default-button:hover {
+  background-color: #0c85d0;
+}
+</style>
